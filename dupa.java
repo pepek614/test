2d7b079ab0dec82e8e7a71b3d683a20f359df87b
pepek614 <pepekr614@gmail.com>
2022-06-30 07:00:46 +0200

    after instaling extension for java

--- dupa.java
+++ dupa.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class dupa{
     public static void main(String[] args){
         System.out.println("dupa, hahah, ale śmieszne");
+        System.out.println("x");
+        List<String> list = new ArrayList<>();
+        list.add("dupa");
+        
     }
 }
